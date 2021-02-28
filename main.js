915de852fb81a47e29d04f93fbfcdf6524a5b082
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-02-28 20:27:26 +0900

    feat : すべてのチェックボックスにチェック

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 window.onload = function() {
-    // 要素を取得
-    var element = document.getElementById( "opt_attend_4" ) ;
-
-    // 選択状態にする
-    element.checked = true ;
+    // ラジオボタンの要素を取得
+    var inputs = document.getElementsByTagName("input");
+    
+    // すべてをチェックする
+    Array.prototype.forEach.call(inputs, function(t){
+        t.checked = true;
+    });
 };
